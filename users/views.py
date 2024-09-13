262a53c8807efab3ea4a9477a5e74a5ec7e8c92f
Hugo Belém <belem.hugo@hotmail.com>
2024-09-13 18:38:34 -0300

    signup and home page

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login
+from django.views.generic import TemplateView
 
-# Create your views here.
+from .forms import CustomUserCreationForm
+
+def signup(request):
+    form = CustomUserCreationForm()
+    
+    if request.method == 'POST': 
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower().strip()
+            user.save()
+            login(request, user)
+            return redirect('users:home')
+
+    return render(request, 'signup.html', {'form': form})
+
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
